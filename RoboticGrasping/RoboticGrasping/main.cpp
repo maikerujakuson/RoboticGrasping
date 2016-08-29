88dd9195229d5ddaaa63a62d562a6be13a47ec83
maikerujakuson <toshitaka_suzuki@outlook.jp>
2016-08-29 15:55:56 +0900

    iarm動作確認テストコードをmainに追加

--- RoboticGrasping/RoboticGrasping/main.cpp
+++ RoboticGrasping/RoboticGrasping/main.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
+#include"iarmInterface.h"
+
+
 
 int main()
 {
+	float x, y, z, roll, pitch, yaw;
+	IARMInterface iarm;
+	iarm.init();
+	iarm.stockUnfold();
+	iarm.stockLiftDown();
+	iarm.stockMoveHomePosition();
+
+	while (true) {
+		if (iarm.check() == IARMInterface::CHECK_RESULT_FINISH) {
+			printf("x y z\n");
+			scanf("%f %f %f", &x, &y, &z);
+			if (x == 1000 || y == 1000 || z == 1000) break;
+			iarm.moveTo(x, y, z);
+		}
+	}
 	return 0;
 }
