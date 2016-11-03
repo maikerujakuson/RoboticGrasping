5aae0dbb8d4cf16a7ec615e189062a5166dca594
toshitaka <toshitaka_suzuki@outlook.jp>
2016-11-03 18:07:13 +0900

    Add requester

--- RoboticGrasping/RoboticGrasping/main.cpp
+++ RoboticGrasping/RoboticGrasping/main.cpp
@@ -48,10 +48,11 @@ struct termios ots; /* copy of initial termios of tty */
 #include "iarm.h"
 
 #include <Eigen/Dense>
+#include <iostream>
 
 
-					/* CONSTANTS */
-					// Enumeration for catesian dimension system
+/* CONSTANTS */
+// Enumeration for catesian dimension system
 typedef enum IARM_CARTESIAN_DIMENSION { X, Y, Z, YAW, PITCH, ROLL } IARM_CARTESIAN_DIMENSION;
 typedef enum IARM_JOINT { J1, J2, J3, J4, J5, J6, GRIPPER } IARM_JOINT;
 
@@ -106,10 +107,20 @@ float positionHome[IARM_NR_JOINTS] = { 129.0f, -420.0f, 7.0f, 1.57f, 0.0f, -1.57
 float positionZero[IARM_NR_JOINTS] = { 240.0f, 0.0f, 0.0f, 1.57f, 1.57f, 0.0f };
 
 // Variable for socket communication
-SOCKET sock;
-struct sockaddr_in dest1;
-char buf[1024];
+// Socket for sending
+SOCKET sock_send;
+// Socket for receiving
+SOCKET sock_recv;
+struct sockaddr_in addr_send, addr_recv;
+char buf[2048];
 WSADATA wsaData;
+// Variable to use select()
+fd_set fds, readfds;
+
+// Vec
+Eigen::Vector4f vec;
+
+
 
 // Cheack if position is valid
 bool isPositionValid(Eigen::Vector4f& position)
@@ -146,24 +157,71 @@ int main(int argc, char *argv[])
 	// Make connections between iARM and PC 
 	g_hRobot = iarm_connect(g_can_port);
 
-	// Initialize socket 
+	// Initialize socket
 	WSAStartup(MAKEWORD(2, 0), &wsaData);
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
-	dest1.sin_family = AF_INET;
-	dest1.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-	dest1.sin_port = htons(11111);
+	sock_recv = socket(AF_INET, SOCK_DGRAM, 0);
+	sock_send = socket(AF_INET, SOCK_DGRAM, 0);
+
+	addr_send.sin_family = AF_INET;
+	addr_recv.sin_family = AF_INET;
+
+	addr_send.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	addr_recv.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+	addr_send.sin_port = htons(11111);
+	addr_recv.sin_port = htons(22222);
+
+	// Bind receiver socket 
+	bind(sock_recv, (struct sockaddr *)&addr_recv, sizeof(addr_recv));
 
 	// Terminate the program when connection is failed
 	if (IARM_INVALID_HANDLE == g_hRobot)
 	{
 		printf("Send\n");
 
-		// Write text to socket
 		memset(buf, 0, sizeof(buf));
-		_snprintf(buf, sizeof(buf), "data to port 11111");
-		// Send socket
-		sendto(sock,
-			buf, strlen(buf), 0, (struct sockaddr *)&dest1, sizeof(dest1));
+		_snprintf(buf, sizeof(buf), "Give me object information!!!!");
+		sendto(sock_send,
+			buf, strlen(buf), 0, (struct sockaddr *)&addr_send, sizeof(addr_send));
+
+		closesocket(sock_send);
+
+		// fd_setの初期化します
+		FD_ZERO(&readfds);
+
+		// selectで待つ読み込みソケットとしてsock1を登録します
+		FD_SET(sock_recv, &readfds);
+
+		// Do listen untile recieving data
+		while (1) {
+			// Initialize fds
+			memcpy(&fds, &readfds, sizeof(fd_set));
+
+			// fdsに設定されたソケットが読み込み可能になるまで待ちます
+			select(0, &fds, NULL, NULL, NULL);
+
+			// sock1に読み込み可能データがある場合
+			if (FD_ISSET(sock_recv, &fds)) {
+				// sock1からデータを受信して表示します
+				memset(buf, 0, sizeof(buf));
+				recv(sock_recv, buf, sizeof(buf), 0);
+				std::string s = buf;
+				std::stringstream ss(s);
+				std::string item;
+				int i = 0;
+				while (std::getline(ss, item, ' ') && !item.empty()) {
+					vec(i++) = stof(item);
+				}
+				std::cout << vec.x() << std::endl;
+				std::cout << vec.y() << std::endl;
+				std::cout << vec.z() << std::endl;
+				std::cout << s << std::endl;
+				//printf("%s\n", buf);
+				break;
+			}
+		}
+
+		WSACleanup();
 
 		// Print error messeage and terminate program
 		printf("Could not open CAN device on CAN-bus %d\n", g_can_port);
@@ -206,10 +264,7 @@ int main(int argc, char *argv[])
 	iarm_disconnect(g_hRobot);
 	printf("iARM terminated.\nPress any key to quit...\n");
 
-	// Clean up socket
-	closesocket(sock);
 
-	WSACleanup();
 
 #ifdef WIN32
 	_getch();
@@ -373,12 +428,6 @@ void process_key_press(char key)
 		printf(" Requesting object vector to the recognizer...");
 		// Variable for the position of the object 
 		Eigen::Vector4f positionOfobject;
-		// Write text to socket
-		memset(buf, 0, sizeof(buf));
-		_snprintf(buf, sizeof(buf), "Request for object vector");
-		// Send socket
-		sendto(sock,
-			buf, strlen(buf), 0, (struct sockaddr *)&dest1, sizeof(dest1));
 
 		if (isPositionValid(positionOfobject)) {
 			break;
